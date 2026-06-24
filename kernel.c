8a61d856b7e3e8bcac2be82c6e2407408756fab2
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-06-24 07:34:34 +0000

    kernel.c

--- kernel.c
+++ kernel.c
@@ -3,8 +3,8 @@ void kernel_main() {
     char *vga = (char *)0xb8000;
     char *mozi = "HAMA OS [Version 1.0.0]";
     int mozisuu = strlen(mozi);
-    for (int a = 0; a<((mozisuu * 2) + 2); a+=2) {
-        vga[a] = mozi[a/2];
-        vga[a+1] = 0x07;
+    for (int a = 0; a<((mozisuu * 2) + 2); a++) {
+        vga[a*2] = mozi[a];
+        vga[a*2+1] = 0x07;
     }
 }
