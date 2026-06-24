22c8851b8a6fcd73eae477df647bd1a7b8dcb822
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-06-24 10:35:16 +0000

    c

--- kernel.c
+++ kernel.c
@@ -1,15 +1,15 @@
 #include "strong.h"
 #include "std.h"
-void kernel_main() {
-    shell()
-}
 void shell() {
     cmd = "help";
     while (1) {
         
 
         if(strcmp(cmd, "help") == 0) {
-            print("help:このヘルプを表示する。")
+            print("help:このヘルプを表示する。");
         }
     }
+}
+void kernel_main() {
+    shell();
 }
