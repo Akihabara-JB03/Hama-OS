22541eac021efb4de09b2beccd484b9d42483429
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-06-25 15:55:33 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,10 +1,19 @@
 #include "strong.h"
 #include "std.h"
+#include "key.h"
 void shell() {
-    char *cmd = "help";
+    char cmd[1024];
+    
+    int pos = 0;
     while (1) {
-        
+        char *c = get_char();
+        if (c) {
+            cmd[pos] = c;
+            pos++;
+            cmd[pos] = '\0';
 
+            print(c);
+        }
         if(strcmp(cmd, "help") == 0) {
             print("help:このヘルプを表示する。");
         }
